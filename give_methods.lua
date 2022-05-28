c226e1c4772f05c2aa8875ee2b88ecf3adbf83ef
ThatGravyBoat <thatgravyboat@gmail.com>
2022-05-28 07:06:34 -0230

    param descs for give methods

--- give_methods.lua
+++ give_methods.lua
@@ -1,15 +1,15 @@
 ---
----@param amount number
+---@param amount number the amount to give
 ---@return void
 function api_give_honeycore(amount)  end
 
 ---
----@param amount number
----@return void
-function api_give_money(amount)  end
-
----
----@param item_oid string
----@param amount number
+---@param item_oid string the item oid to give
+---@param amount number the amount to give
 ---@return void
 function api_give_item(item_oid, amount)  end
+
+---
+---@param amount number the amount to give
+---@return void
+function api_give_money(amount)  end
