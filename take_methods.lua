e69f4906657c9029f6a90d6bb189fb6282dbba2f
ThatGravyBoat <thatgravyboat@gmail.com>
2022-05-28 07:52:29 -0230

    added param desc to take and use methods

--- take_methods.lua
+++ take_methods.lua
@@ -1,9 +1,9 @@
 ---
----@param amount number
+---@param amount number the amount to take
 ---@return void
 function api_take_honeycore(amount)  end
 
 ---
----@param amount number
+---@param amount number the amount to take
 ---@return void
 function api_take_money(amount)  end
--- use_methods.lua
+++ use_methods.lua
@@ -1,10 +1,10 @@
 ---
----@param item_oid string
----@param amount number
+---@param item_oid string the item to use up
+---@param amount number the amount to use up
 ---@return void
 function api_use_item(item_oid, amount)  end
 
 ---
----@param item_oid string
+---@param item_oid string the item to use up
 ---@return number total amount of item in player inventory and opened menus
 function api_use_item(item_oid)  end
