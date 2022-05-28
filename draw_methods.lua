829400ab64b8906b9c7424e051bd9d55eb1d66a0
ThatGravyBoat <thatgravyboat@gmail.com>
2022-05-28 06:52:46 -0230

    add param desc to draw methods

--- draw_methods.lua
+++ draw_methods.lua
@@ -1,90 +1,90 @@
 ---
----@param button_id number
----@param show_text boolean
+---@param button_id number the button id to draw
+---@param show_text boolean if the button has text you can choose to draw it here
 ---@return void
 function api_draw_button(button_id, show_text)  end
 
 ---
----@param cx number
----@param cy number
----@param rad number
----@param outline boolean
----@param col string [Optional]
----@param alpha number [Optional]
+---@param cx number the x position to draw at (this will be at the centre of the circle)
+---@param cy number the y position to draw at (this will be at the centre of the circle)
+---@param rad number the radius of the circle
+---@param outline boolean whether to draw an outline instead of a filled circle
+---@param col string [Optional] the color key to use for this circle color, either one you defined with api_define_color() or a name from the colors_ref.json game file, defaults to white if not specified
+---@param alpha number [Optional] the alpha level to draw with (number between 0-1)
 ---@return void
 function api_draw_circle(cx, cy, rad, outline, col, alpha)  end
 
 ---
----@param x1 number
----@param y1 number
----@param x2 number
----@param y2 number
----@param col string [Optional]
----@param alpha number [Optional]
+---@param x1 number the x position to start the line at
+---@param y1 number the y position to start the line at
+---@param x2 number the x position to end the line at
+---@param y2 number the y position to end the line at
+---@param col string [Optional] the color key to use for this circle color, either one you defined with api_define_color() or a name from the colors_ref.json game file, defaults to white if not specified
+---@param alpha number [Optional] the alpha level to draw with (number between 0-1)
 ---@return void
 function api_draw_line(x1, y1, x2, y2, col, alpha)  end
 
 ---
----@param x1 number
----@param y1 number
----@param x2 number
----@param y2 number
----@param col string
----@param outline boolean
----@param alpha number [Optional]
+---@param x1 number the left x position of the rectangle
+---@param y1 number the top y position of the rectangle
+---@param x2 number the right x position of the rectangle
+---@param y2 number the bottom y position of the rectangle
+---@param col string the color key to use for this circle color, either one you defined with api_define_color() or a name from the colors_ref.json game file, defaults to white if set as nil
+---@param outline boolean whether to draw an outline instead of a filled rectangle
+---@param alpha number [Optional] the alpha level to draw with (number between 0-1)
 ---@return void
 function api_draw_rectangle(x1, y1, x2, y2, col, outline, alpha)  end
 
 ---
----@param menu_id number
+---@param menu_id number the menu instance to draw the slots for (NOT the menu object instance)
 ---@return void
 function api_draw_slots(menu_id)  end
 
 ---
----@param sprite_ref number
----@param frame number
----@param sx number
----@param sy number
+---@param sprite_ref number the sprite id to draw with
+---@param frame number the sprite frame to draw
+---@param sx number the x position to draw at
+---@param sy number the y position to draw at
 ---@return void
 function api_draw_sprite(sprite_ref, frame, sx, sy)  end
 
 ---
----@param sprite_ref number
----@param frame number
----@param left number
----@param top number
----@param width number
----@param height number
----@param sx number
----@param sy number
+---@param sprite_ref number the sprite id to draw with
+---@param frame number the sprite frame to draw
+---@param left number the x position on the sprite image to draw the part from
+---@param top number the y position on the sprite image to draw the part from
+---@param width number the width of the part
+---@param height number the height of the part
+---@param sx number the x position to draw at
+---@param sy number the y position to draw at
 ---@return void
 function api_draw_sprite_part(sprite_ref, frame, left, top, width, height, sx, sy)  end
 
 ---
----@param sprite_ref number
----@param frame number
----@param sx number
----@param sy number
----@param x_scale number
----@param y_scale number
----@param rot number
----@param col string
----@param alp number
+---@param sprite_ref number the sprite id to draw with
+---@param frame number the sprite frame to draw
+---@param sx number the x position to draw at
+---@param sy number the y position to draw at
+---@param x_scale number the x scale to apply
+---@param y_scale number the y scale to apply
+---@param rot number the rotation angle to draw at
+---@param col string the color blend to apply, use nil to apply no color blending
+---@param alp number the alpha level to draw with, a number between 0-1
 ---@return void
 function api_draw_sprite_ext(sprite_ref, frame, sx, sy, x_scale, y_scale, rot, col, alp)  end
 
 ---
----@param tx number
----@param ty number
----@param text string
----@param card boolean
----@param col string
----@param tw number
+---@param tx number the x position to draw at
+---@param ty number the y position to draw at
+---@param text string the text to draw, can use | to go to a new line if tw is set
+---@param card boolean whether to show a card background behind the text (like tooltips)
+---@param col string [Optional] the color key to use for this circle color, either one you defined with api_define_color() or a name from the colors_ref.json game file, defaults to white if not specified
+---@param tw number [Optional] a width to apply to the text, causing extra text to wrap onto the next line
 ---@return void
 function api_draw_text(tx, ty, text, card, col, tw)  end
 
 ---
----@param tank_id number
+---@param tank_id number the tank gui id to draw
 ---@return void
 function api_draw_tank(tank_id)  end
 
