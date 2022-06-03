cd29d75df2b661a129bb80a1aa9d0531b6c81b2f
ThatGravyBoat <thatgravyboat@gmail.com>
2022-06-03 01:44:32 -0230

    Fixed use total not being supplied

--- use_methods.lua
+++ use_methods.lua
@@ -6,4 +6,4 @@ function api_use_item(item_oid, amount)  end
 ---
 ---@param item_oid string the item to use up
 ---@return number total amount of item in player inventory and opened menus
-function api_use_item(item_oid)  end
+function api_use_total(item_oid)  end
